19e60de00bc5b05e743de1c273e42501b1366c08
jiayee <jcho0156@student.monash.edu>
2024-06-14 11:10:10 +0800

    working home page responsive

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './Home.scss';
+import cuteImage from '../../assets/cute.png';
 
 export default function Home(){
     return(
         <div className="home">
-            <h1>Home</h1>
+            <div className="desc"> 
+            <h1>My Portfolio.</h1>
+            <h2>Hi there!</h2>
+            <h4>My name is Chong Jia Yee.</h4>
+            <hr></hr>
+            <p>I'm a year 2 computer science student. </p>
+            <p>Looking for internship in software development. </p>
+            <p>Currently have 4.0 cgpa and scored 9A+ in SPM. </p>
+            </div>
+            <div className='img'>
+                <img src={cuteImage} alt="profile" />
+            </div>
+            
         </div>
     )
 }
