80f0809ced38a5564bf4a3cb300cdec2191862a7
jiayee <jcho0156@student.monash.edu>
2024-06-14 14:44:20 +0800

    achievements working

--- src/components/Achievements/Achievements.js
+++ src/components/Achievements/Achievements.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import './Achievements.scss';
+import './Achievements.scss'; // Include your CSS/SCSS file for styling
+import achievementsData from './Achievements.json'; // Import your achievements data
 
-export default function Achievements(){
-    return(
+export default function Achievements() {
+    return (
         <div className="achievements">
-            <h1>Achievements</h1>
+            {/* <h1>Achievements</h1> */}
+            <div className="timeline">
+                {achievementsData.achievements.map((achievement, index) => (
+                    <div key={index} className="achievement">
+                        <div className="date">{achievement.date}</div>
+                        <div className="content">
+                            <h2>{achievement.title}</h2>
+                            <p>{achievement.description}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
-    )
+    );
 }
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import './Contact.scss';
+import { AiOutlineMail } from 'react-icons/ai';
+import { FaLinkedin } from 'react-icons/fa';
+import { AiFillGithub } from 'react-icons/ai';
+import { AiOutlinePhone } from 'react-icons/ai';
 
-export default function Contact(){
-    return(
+export default function Contact() {
+    return (
         <div className="contact">
-            <h1>Contact</h1>
+            <div className="description">
+                {/* <h5>Feel free to find me at:</h5> */}
+            </div>
+            <div className="apps">
+                <div className="app gmail">
+                    <AiOutlineMail className="icon" />
+                    <a href="mailto:chongjyee@gmail.com">Gmail</a>
+                </div>
+                <div className="app linkedin">
+                    <FaLinkedin className="icon" />
+                    <a href="www.linkedin.com/in/jia-yee-chong-2a5a60285">LinkedIn</a>
+                </div>
+                <div className="app github">
+                    <AiFillGithub className="icon" />
+                    <a href="https://github.com/chongjiayee0156">GitHub</a>
+                </div>
+                <div className="app phone">
+                    <AiOutlinePhone className="icon" />
+                    <a href="tel:+0173319897">Phone</a>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
