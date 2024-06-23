43be2558c29c594c68eb1e7ec7ae5e6b8c363082
jcho0156 <jcho0156@student.monash.edu>
2024-06-24 01:19:13 +0800

    added projects

--- src/components/Projects/Projectsdata.js
+++ src/components/Projects/Projectsdata.js
@@ -3,6 +3,9 @@ import tictactoeimg from "../../assets/tictactoeimg.png";
 import creditcardimg from "../../assets/creditcardimg.png";
 import portfolioimg from "../../assets/portfolioimg.png";
 import visualizerimg from "../../assets/visualizerimg.png";
+import organizationtasktrackingimg from "../../assets/organizationtasktrackingimg.png";
+import responsivesinglepagewebsiteimg from "../../assets/responsivesinglepagewebsiteimg.png";
+import animatedcandleimg from "../../assets/animatedcandleimg.png";
 
 export const projectsData = [
     {
@@ -17,17 +20,27 @@ export const projectsData = [
         "name": "Sorting Algorithm Visualizer",
         "img": visualizerimg,
         "description": "A sorting algorithm revision website, includes visualizer, code, complexity and explanation of the algorithm",
+        "techstack": "html, css, javascript, data structure and algorithm",
         "github": "https://github.com/chongjiayee0156/Sorting-Algo-Visualizer",
         "demo": "https://chongjiayee0156.github.io/Sorting-Algo-Visualizer/"
     },
     {
-        "name": "Portfolio Website",
-        "img": portfolioimg,
-        "description": "A portfolio website to showcase my projects and skills",
-        "techstack": "React, HTML, CSS, JavaScript, Bootstrap, GitHub Pages",
-        "github": "",
-        "demo": ""
-    },{
+        "name": "Fullstack Task Tracker",
+        "img": organizationtasktrackingimg,
+        "description": "Utilized 3 servers for backend, frontend, database. A task tracker for organization, includes authentication, authorization, CRUD operations, and task tracking features",
+        "techstack": "React, node, express, redux, mongodb",
+        "github": "https://github.com/chongjiayee0156/fullstack-note-tracking",
+        "demo": "https://swiftnote-tpdp.onrender.com/"
+    },
+    {
+        "name": "Animated Glowing Candle",
+        "img": animatedcandleimg,
+        "description": "Used css to code from scartch, an animated glowing candle on a birthday cake",
+        "techstack": "css, html",
+        "github": "https://github.com/chongjiayee0156/bday-website",
+        "demo": "https://chongjiayee0156.github.io/bday-website/"
+    },
+{
         "name": "Tic Tac Toe Game",
         "img": tictactoeimg,
         "description": "A simple Tic Tac Toe game website",
@@ -41,5 +54,19 @@ export const projectsData = [
         "techstack": "HTML, CSS, 3D animation",
         "github": "https://github.com/chongjiayee0156/flipping-credit-card",
         "demo": "https://chongjiayee0156.github.io/flipping-credit-card/"
+    },{
+        "name": "Portfolio Website",
+        "img": portfolioimg,
+        "description": "A portfolio website to showcase my projects and skills",
+        "techstack": "React, HTML, CSS, JavaScript, Bootstrap, GitHub Pages",
+        "github": "",
+        "demo": ""
+    },{
+        "name": "Single Responsive Rocket Website",
+        "img": responsivesinglepagewebsiteimg,
+        "description": "A responsive single page website",
+        "techstack": "HTML, CSS, JavaScript",
+        "github": "",
+        "demo": ""
     }
 ]
